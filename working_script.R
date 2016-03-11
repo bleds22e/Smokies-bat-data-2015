68553799b9b045f048ecbd3956670dabd2120da1
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2016-03-11 10:34:43 -0500

    Just use dplyr to do all of this

--- working_script.R
+++ working_script.R
@@ -9,6 +9,8 @@ files <-  list.files("data_csv", pattern="*.csv", full.names = TRUE)
 for (i in 1:length(files)) assign(files[i], read.csv(files[i]))
 # is there a way to get rid of the "data_csv" part at the beginning?
 
+# jk just use dplyr--then you can export into csv and stuff :)
+
 ##### Functions #####
 
 check_channel <- function(data, channel){
@@ -61,15 +63,23 @@ check_NA_BPM <- function(data){
   print("done")
 }
 
+###### working
 BPMchange <- function(data){
   # lists row numbers for BPM change > 1
   for (n in 1:length(data[,1])) {
     if (data$BPM.Change[n] == 1) {
-      print(data$ref[n])
+      issues <- print(data$ref[n])
     }
   }
-  print("done")
+  return(issues)
 }
+
+BPMchange(`data_csv/MYSE_151909.csv`)
+issues <- as.list(BPMchange(`data_csv/MYSE_151909.csv`))
+print(paste('BPM_change_1:', issues))
+
+##################
+
 
 ##### Results #####
 
@@ -82,74 +92,4 @@ time_1 <- time_change(data)
 BPM_NA <- check_NA_BPM(data)
 BPM_1 <- BPMchange(data)
 
-# MYSE 151.909
-data <- myse_1909
-channel <- check_channel(data, data$Channel[2])
-timechange_NA <- check_NA_timechange(data)
-time_NA <- check_NA_time(data)
-time_1 <- time_change(data)
-BPM_NA <- check_NA_BPM(data)
-BPM_1 <- BPMchange(data)
-
-# MYSO 150.995
-data <- myso_0995
-channel <- check_channel(data, data$Channel[2])
-timechange_NA <- check_NA_timechange(data)
-time_NA <- check_NA_time(data)
-time_1 <- time_change(data)
-BPM_NA <- check_NA_BPM(data)
-BPM_1 <- BPMchange(data)
-
-# MYSE 151.230
-data <- myse_1230
-channel <- check_channel(data, data$Channel[2])
-timechange_NA <- check_NA_timechange(data)
-time_NA <- check_NA_time(data)
-time_1 <- time_change(data)
-BPM_NA <- check_NA_BPM(data)
-BPM_1 <- BPMchange(data)
-
-# MYSE 151.312
-data <- myse_1312
-channel <- check_channel(data, data$Channel[2])
-timechange_NA <- check_NA_timechange(data)
-time_NA <- check_NA_time(data)
-time_1 <- time_change(data)
-BPM_NA <- check_NA_BPM(data)
-BPM_1 <- BPMchange(data)
-
-# MYSO 151.547
-data <- myso_1547
-channel <- check_channel(data, data$Channel[2])
-timechange_NA <- check_NA_timechange(data)
-time_NA <- check_NA_time(data)
-time_1 <- time_change(data)
-BPM_NA <- check_NA_BPM(data)
-BPM_1 <- BPMchange(data)
-
-# MYSE 151.467
-data <- myse_1467
-channel <- check_channel(data, data$Channel[2])
-timechange_NA <- check_NA_timechange(data)
-time_NA <- check_NA_time(data)
-time_1 <- time_change(data)
-BPM_NA <- check_NA_BPM(data)
-BPM_1 <- BPMchange(data)
-
-# MYSE 151.830
-data <- myse_1830
-channel <- check_channel(data, data$Channel[2])
-timechange_NA <- check_NA_timechange(data)
-time_NA <- check_NA_time(data)
-time_1 <- time_change(data)
-BPM_NA <- check_NA_BPM(data)
-BPM_1 <- BPMchange(data)
-
-# MYSO 151.786
-data <- myso_1786
-channel <- check_channel(data, data$Channel[2])
-timechange_NA <- check_NA_timechange(data)
-time_NA <- check_NA_time(data)
-time_1 <- time_change(data)
-BPM_NA <- check_NA_BPM(data)
-BPM_1 <- BPMchange(data)
+# good output for these? text file or csv?
