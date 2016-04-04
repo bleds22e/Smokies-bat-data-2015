74bbdf9acffdd1260cac6c248f40640d6343a8ca
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2016-04-04 11:55:30 -0400

    New script of 2014 bats

---
+++ 2014_data.R
@@ -0,0 +1,27 @@
+#2014 Smokies Bat Data
+  # includes day-time data logger data, body temp 
+  # and outside temps
+# Ellen Bledsoe
+# started April 4, 2016
+
+
+# LOAD LIBRARIES 
+####################################
+
+library(dplyr)
+
+
+# LOAD FILES
+####################################
+
+hobo_data <- read.csv("2014bats/2014_hobo_data.csv") 
+myse_073 <- read.csv("2014bats/myse_073.csv")
+myse_189 <- read.csv("2014bats/myse_189.csv")
+myse_227 <- read.csv("2014bats/myse_227.csv")
+myse_456 <- read.csv("2014bats/myse_456.csv")
+myso_313 <- read.csv("2014bats/myso_313.csv")
+
+
+# ARRANGE THE DATA
+####################################
+
