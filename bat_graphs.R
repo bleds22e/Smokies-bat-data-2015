85f2a7df976c66047be958b3a4ab7b894d0e1bc3
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2016-06-30 00:41:43 -0400

    for loop edits

--- bat_graphs.R
+++ bat_graphs.R
@@ -137,19 +137,20 @@ unique_id_total <- select(bat_data, bat_id) %>% distinct()
   # make individual df, put in list, the lapply/for loop through list
   # OR
   # use bat_data and 
-unique_id_temp <- as.list(unique_id_temp)
 
 # could use bat id as facet wrap in ggplot (even divide by species, sex, etc)
-# individual plots using for loop based on unique id vector
-for batID in unique_id_temp{
-  plot_torpor(bat = bat_data[bat_data$bat_id == batID], weather = weather_data[weather$bat_id == batID])
+# individual plots using for loop based on unique id vector (list?)
+for bat in unique_id{
+  bat_data <- filter(bat_data, bat_id == bat)
+  weather_data <- filter(weather_data, bat_id == bat)
+  plot_torpor(bat_data, weather_data)
 }
 
 #######################
 # NOT CURRENTLY IN USE
 
 ### bat data
-#filenames <- list.files(path = "barney_data", pattern = "bat_", full.names = FALSE)
+filenames <- list.files(path = "barney_data", pattern = "bat_", full.names = FALSE)
 #bat_data <- list()
 #for (file in filenames){
   # make each bat a dataframe in a list called bat_data
